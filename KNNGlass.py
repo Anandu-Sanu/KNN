1755291869b1b8c1e8058ef659b03b859016a7cc
worldonthe_rim <61133523+Anandu-Sanu@users.noreply.github.com>
2020-03-08 14:21:23 +0530

    Add files via upload

---
+++ KNNGlass.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 14:01:15 2020
+
+@author: Anandhu Sanu
+"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+glass = pd.read_csv("F:\\ExcelR\\excelRASS\\ass8 KNN\\glass.csv")
+glass.dtypes
+
+feature_cols=['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe']
+x=glass[feature_cols]
+y=glass.Type
+print(x.shape)
+print(y.shape)
+xtrain,ytrain,xtest,ytest=train_test_split(x,y,test_size=0.3,random_state=1)
+xtrain.shape
+ytrain.shape
+knn=KNeighborsClassifier(n_neighbors=5)
+knn.fit(xtrain,ytrain)
